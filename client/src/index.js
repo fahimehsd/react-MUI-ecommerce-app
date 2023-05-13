ec967a421df58d2952cafcc11e89fb6214673728
fahimehsd <fahimehsadeghi3@gmail.com>
2023-05-13 19:36:40 +0330

    redux added

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,16 @@ import App from "./App";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./theme";
 import { CssBaseline } from "@mui/material";
+import { Provider } from "react-redux";
+import { store } from "./state/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
